0ec1f31df885863b4fed16872c8f5dba39b5b2b2
Colin Hoolwerf <choolwerf@gmail.com>
2022-03-12 10:58:20 -0600

    progress! created the CRUD app functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { hot } from 'react-hot-loader/root';
 import PostList from "./PostList";
+import PostForm from "./PostForm";
 
 const App = ({ name }) => {
 
   return (
-    <>
+    <div>
+      <PostForm />
       <PostList />
-    </>
+    </div>
   );
 }
 
--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,44 @@
 export const getPosts = async () => {
-    const url = 'https://jsonplaceholder.typicode.com/posts/';
+    const url = 'https://strangers-things.herokuapp.com/api/2112-FTW-ET-WEB-PT/posts';
     const response = await fetch(url)
     const json = await response.json()
     return json;
-} 
+};
+
+export const createNewPost = async (newPost) => {
+    const url = 'https://strangers-things.herokuapp.com/api/2112-FTW-ET-WEB-PT/posts';
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newPost)
+    });
+
+    const json = await response.json();
+    return json;
+};
+
+export const updatePost = async (postToUpdate) => {
+    const url = 'https://strangers-things.herokuapp.com/api/2112-FTW-ET-WEB-PT/posts';
+    const response = await fetch(url, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postToUpdate)
+    });
+
+    const json = await response.json();
+    return json;
+};
+
+export const deletePost = async (postId) => {
+    const url = `https://strangers-things.herokuapp.com/api/2112-FTW-ET-WEB-PT/posts/${postId}`;
+    const response = await fetch(url, {
+        method: "DELETE"
+    });
+
+    const json = await response.json();
+    return json;
+};
