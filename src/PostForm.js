5c02da5b1e3ba8a692df503f87c4d82655eadf99
Colin Hoolwerf <choolwerf@gmail.com>
2022-03-15 20:57:18 -0500

    help me....

--- src/PostForm.js
+++ src/PostForm.js
@@ -3,34 +3,54 @@ import { createNewPost } from "./api";
 
 const PostForm = () => {
   const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState();
+  const [location, setLocation] = useState("");
+  const [willDeliver, setWillDeliver] = useState("");
 
   const handlePost = () => {
     const postInfo = {
       title: title,
-      body: body,
+      description: description,
+      price: price,
+      location: location,
+      willDeliver: false,
     };
     createNewPost(postInfo);
     setTitle("");
-    setBody("");
+    setDescription("");
+    setPrice("");
+    setLocation("");
+    setWillDeliver("");
   };
 
   const handleTitle = (e) => {
     setTitle(e.target.value);
   };
 
-  const handleBody = (e) => {
-    setBody(e.target.value);
+  const handleDesc = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handlePrice = (e) => {
+    setPrice(e.target.value);
+  };
+
+  const handleLocation = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const handleDeliver = (e) => {
+    setWillDeliver(e.target.value);
   };
 
   return (
     <div>
-      <input
-        placeholder="Title Here"
-        value={title}
-        onChange={handleTitle}
-      ></input>
-      <input placeholder="Body Here" value={body} onChange={handleBody}></input>
+      <input placeholder="Title Here" value={title} onChange={handleTitle}></input>
+      <input placeholder="Description Here" value={description} onChange={handleDesc}></input>
+      <input placeholder="Price Here" value={price} onChange={handlePrice}></input>
+      <input placeholder="Your Location" value={location} onChange={handleLocation}></input>
+      <input placeholder="Are You Willing to Deliver?" value={willDeliver} onChange={handleDeliver}></input>
       <button onClick={handlePost} styles={{ width: "100px", height: "50px" }}>
         Submit
       </button>
